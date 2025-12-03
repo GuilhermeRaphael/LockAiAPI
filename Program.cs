54e1f875cf07a67c303243ae7c5d21353212f742
cabrera0laura <lucas56hoseok@gmail.com>
2025-12-03 14:13:58 -0300

    atualizando

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddControllers()
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<DataContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoLocal")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoAzure")));
 
 builder.Services.AddCors(options =>
 {
